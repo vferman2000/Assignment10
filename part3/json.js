240d524e64c5b25f7f8e6b0c1d8ef4289bf8f06d
vferman2000 <vferman2000@gmail.com>
2017-05-03 21:31:30 -0700

    Add files via upload

---
+++ part3/json.js
@@ -0,0 +1,27 @@
+{"locations":[
+	{
+		"country":"Spain",
+		"city":"Madrid",
+		"summary":"The city has a population of almost 3.2 million with a metropolitan area population of approximately 6.5 million. It is the third-largest city in the European Union after London and Berlin."
+	},
+	{
+		"country":"France",
+		"city":"Paris",
+		"summary":"By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today. The Paris Region accounts for 30.0 percent of the GDP of France and ranking it as one of the wealthiest regions in Europe."
+	},
+	{
+		"country":"England",
+		"city":"London",
+		"summary":"London is a leading global city[17][18] in the arts, commerce, education, entertainment, fashion, finance, healthcare, media, professional services, research and development, tourism, and transportation.[19][20][21] It is crowned as the world's largest financial centre."
+	},
+	{
+		"country":"Argentina",
+		"city":"Buenos Aires",
+		"summary":"Buenos Aires is a top tourist destination, and is known for its preserved Spanish/European-style architecture and rich cultural life. Buenos Aires held the 1st Pan American Games in 1951 as well as hosting two venues in the 1978 FIFA World Cup."
+	},
+	{
+		"country":"Austria",
+		"city":"Vienna",
+		"summary":"Apart from being regarded as the City of Music[14] because of its musical legacy, Vienna is also said to be the city of Dreams because it was home to the world's first psycho-analyst – Sigmund Freud. The city's roots lie in early Celtic and Roman settlements that transformed into a Medieval and Baroque city, and then the capital of the Austro-Hungarian Empire"
+	}
+]}
